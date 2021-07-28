5d060618bd16ace15eda6224fa626bbb3b91bc68
soon.good <playmymind@daum.net>
2021-07-28 21:34:06 +0900

    fanout 예제 프로젝트 추가

---
+++ study-rabbitmq-fanout/src/main/java/io/study/studyrabbitmqfanout/StudyRabbitmqFanoutApplication.java
@@ -0,0 +1,13 @@
+package io.study.studyrabbitmqfanout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRabbitmqFanoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRabbitmqFanoutApplication.class, args);
+	}
+
+}
---
+++ study-rabbitmq-fanout/src/test/java/io/study/studyrabbitmqfanout/StudyRabbitmqFanoutApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.studyrabbitmqfanout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRabbitmqFanoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
