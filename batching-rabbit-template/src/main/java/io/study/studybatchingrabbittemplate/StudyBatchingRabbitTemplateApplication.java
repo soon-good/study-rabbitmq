e4131f09b0d833b6d260e116fdf5d12ab0a7f3e6
soon.good <playmymind@daum.net>
2021-08-01 14:24:34 +0900

    BatchingRabbitTemplate 테스트를 위한 예제 프로젝트 추가

---
+++ batching-rabbit-template/src/main/java/io/study/studybatchingrabbittemplate/StudyBatchingRabbitTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.study.studybatchingrabbittemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBatchingRabbitTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBatchingRabbitTemplateApplication.class, args);
+	}
+
+}
