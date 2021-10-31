1028cc56cee232996596e222725f03b7ad3e94a9
soon.good <playmymind@daum.net>
2021-10-31 11:22:09 +0900

    stomp fanout 예제 추가

---
+++ stomp-fanout/src/main/java/io/study/stompfanout/StompFanoutApplication.java
@@ -0,0 +1,13 @@
+package io.study.stompfanout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompFanoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StompFanoutApplication.class, args);
+	}
+
+}
